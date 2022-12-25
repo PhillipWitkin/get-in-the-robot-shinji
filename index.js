f502ea78e062a58bd4b8a89f11420978ad8fb510
Phillip Witkin <pwitkin273@gmail.com>
2022-12-24 19:49:00 -0500

    added comments and instructions for second function, mapCompatiblePilotsByUnit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /*
 We have a collection of synchronization test results, 
-each one containing human pilots and a biomechanical Eva Unit they can synchronize with at the time, 
-where each test is respresnted as a map of pilot keys to Eva values
+each one containing the test results for a human pilot and a biomechanical Eva Unit they can synchronize with at the time, 
+where each test is respresnted as a map of of key-vlaue pairs with a pilot key to an Eva Unit value
+
+1. Complete the function mapCompatibleUnitsByPilot:
 
 We want to take in an array of those tests, and respresent that data by a Map of pilots  
 to a Set of all the Eva units they are compatible of synchronizing with 
@@ -12,11 +14,7 @@ For example:
 [
   Map(2) { 'Rei' => 'Eva-00', 'Shinji' => 'Eva-01' },
   Map(2) { 'Asuka' => 'Eva-02', 'Shinji' => 'Eva-01' },
-  Map(3) {
-    'Shinji' => 'Eva-00',
-    'Rei' => 'Eva-01',
-    'Asuka' => 'Eva-02'
-  }
+  Map(3) { 'Shinji' => 'Eva-00', 'Rei' => 'Eva-01', 'Asuka' => 'Eva-02' }
 ]
 
 Should return:
@@ -25,15 +23,40 @@ Map(3) {
   'Shinji' => Set(2) { 'Eva-01', 'Eva-00' },
   'Asuka' => Set(1) { 'Eva-02' }
 }
+
+It should be able to run in O(n) time, where n = total number of k-v pairs in all the input maps, and O(n) space 
+
+
+2. Complete the function mapCompatiblePilotsByUnit:
+
+We want to take the exact same input, but return a map this time as a Map of Eva units to 
+a Set of all the pilots that can synchronize with that Eva. Just Like before, we can use a Set because 
+we don't want duplicate pilots or care about their order. 
+
+The same input
+[
+  Map(2) { 'Rei' => 'Eva-00', 'Shinji' => 'Eva-01' },
+  Map(2) { 'Asuka' => 'Eva-02', 'Shinji' => 'Eva-01' },
+  Map(3) { 'Shinji' => 'Eva-00', 'Rei' => 'Eva-01', 'Asuka' => 'Eva-02' }
+]
+
+Should return:
+Map(3) {
+  'Eva-00' => Set(2) { 'Rei', 'Shinji' },
+  'Eva-01' => Set(2) { 'Shinji', 'Rei' },
+  'Eva-02' => Set(1) { 'Asuka' }
+}
+
+It should also run in O(n) time, where n = total number of k-v pairs in all the input maps, and O(n) space
 */
 
 
 // takes in an array of Map objects
 // returns a Map object whose keys match all the input map keys, 
 // and value holds a Set with all matching values for that key among all maps
-const convertArrayOfMapsToMapOfSets = (pilotTests) => {
+function mapCompatibleUnitsByPilot(pilotTests) {
 
-    mapUnitsByPilot = new Map();
+    const mapUnitsByPilot = new Map();
 
     // iterate through the array of syncTests 
     for (const syncTest of pilotTests){
@@ -60,6 +83,36 @@ const convertArrayOfMapsToMapOfSets = (pilotTests) => {
     return mapUnitsByPilot;
 }
 
+// takes in an array of Map objects
+// returns a Map object whose keys match all the input map values, 
+// and values holds a Set with all matching keys for that value among all maps
+function mapCompatiblePilotsByUnit(pilotTests) {
+
+    const mapPilotsByUnit = new Map();
+
+    for (const syncTest of pilotTests){
+
+        for (const [pilot, unit] of syncTest){
+            // check to see if that unit is in the final map
+            if (mapPilotsByUnit.has(unit)){
+                // get a reference to the set of compatible pilots
+                compatiblePilots = mapPilotsByUnit.get(unit);
+                // add the pilot for that unit to the set of compatible pilots 
+                compatiblePilots.add(pilot);
+
+            } else {
+                // create a new set for that pilots compatible Evas, and add the Pilot from that sync test
+                const compatiblePilots = new Set();
+                compatiblePilots.add(pilot);
+                // add the set for that pilot as a key-value pair to the final map
+                mapPilotsByUnit.set(unit, compatiblePilots);
+            }
+        }
+    }
+
+    return mapPilotsByUnit;
+}
+
 
 /// we need individual Map objects to put inside the input array
 
@@ -83,8 +136,7 @@ const syncTest3 = new Map(Object.entries({
 
 const syncTestsArray = [syncTest1, syncTest2, syncTest3];
 console.log("input:", syncTestsArray);
-const syncTestsByPilot = convertArrayOfMapsToMapOfSets(syncTestsArray);
-console.log("returned:", syncTestsByPilot);
-
+console.log("mapCompatinleUnitsByPilot:", mapCompatibleUnitsByPilot(syncTestsArray));
+console.log("mapCompatinlePilotsByUnit:", mapCompatiblePilotsByUnit(syncTestsArray));
 
 
